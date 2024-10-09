ede88b3ebbddd9efc92ef84138b76841011298a9
NitisriGirkala <nitisrigirkala@gmail.com>
2024-10-08 22:41:57 -0500

    first commit

---
+++ chat/src/main/java/com/Niti/chat/chat/ChatController.java
@@ -0,0 +1,13 @@
+package com.Niti.chat.chat;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    public ChatMessage sendMessage(
+        @Payload ChatMessage chatMessage
+    ){
+
+    }
+}
